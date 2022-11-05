a03d80d77ca9727ae5b3ed5593a0df0cb6e69f6f
shreyasmoily <shreyasmoily9@gmail.com>
2022-11-05 21:43:45 +0530

    first commit

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CoderMoily from './components/coderMoily';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <CoderMoily/>
+    </div>
+  );
+}
+
+export default App;
---
+++ dojo-blog/src/components/coderMoily.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CoderMoily = () => {
+  return (
+    <div>
+        <h1>Iam Coder moily</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque beatae nostrum, alias quidem repudiandae, a tenetur illo numquam asperiores ab repellat harum reprehenderit aspernatur, placeat mollitia? Itaque reprehenderit nam animi!
+        </p>
+    </div>
+  )
+}
+
+export default CoderMoily;
